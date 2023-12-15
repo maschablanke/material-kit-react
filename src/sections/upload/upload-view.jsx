ce406e152dd61837b0e48af95331a3be424f577a
Mascha Blanke <maschablanke@gmx.de>
2023-12-15 16:40:58 +0100

    trade republic input

--- src/sections/upload/upload-view.jsx
+++ src/sections/upload/upload-view.jsx
@@ -4,6 +4,9 @@ import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
+import { UserView } from 'src/sections/user/view';
+
+
 const VisuallyHiddenInput = styled('input')({
   clip: 'rect(10 10 10 10)',
   clipPath: 'inset(50%)',
@@ -21,6 +24,7 @@ export default function UploadView() {
     <Button component="label" variant="contained" startIcon={<CloudUploadIcon />}>
       Upload file
       <VisuallyHiddenInput type="file" />
+      <UserView />
     </Button>
   );
 }
