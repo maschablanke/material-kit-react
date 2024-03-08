290fe774fe36cc909d6064d30a17cf2cd284b3da
Mascha Blanke <maschablanke@gmx.de>
2024-03-08 15:15:30 +0100

    ungewiss

--- src/sections/upload/dataInputTest.jsx
+++ src/sections/upload/dataInputTest.jsx
@@ -1,43 +1,18 @@
-import * as React from 'react';
-import { useEffect , useState} from 'react';
+import {  useState, useEffect } from 'react';
+
 import List from '@mui/material/List';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
-
-// const options = [
-//   'Show some love to MUI',
-//   'Show all notification content',
-//   'Hide sensitive notification content',
-//   'Hide all notification content',
-// ];
+import MenuItem from '@mui/material/MenuItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemButton from '@mui/material/ListItemButton';
 
 
-export default function SimpleListMenu() {
-    
-    const [dataOptions, setAllDataPages] = useState([]);
+export default function SimpleListMenu(dataOptions, setSelectedIndex, selectedIndex) {
 
-    useEffect(() => {
-      // url die gelesen wird
-      fetch('http://localhost:8080/factoryIds')
-        // ist ne json datei
-        .then((response) => response.json())
-        // nimm die daten die(momentan noch) im data.config.baseUrls array stehen
-        .then((data) => {
-          console.log(data);
-          setAllDataPages(data);
-        })
-        // exeption
-        .catch((err) => {
-          console.log(err.message);
-        });
-    }, []);
+  const [anchorEl, setAnchorEl] = useState(null);
 
-
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [selectedIndex, setSelectedIndex] = React.useState();
   const open = Boolean(anchorEl);
+
   const handleClickListItem = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -51,13 +26,14 @@ export default function SimpleListMenu() {
     setAnchorEl(null);
   };
 
+  
   return (
     <div>
       <List
         component="nav"
         aria-label="Device settings"
         sx={{ bgcolor: 'background.paper' }}
-      >
+        >
         <ListItemButton
           id="lock-button"
           aria-haspopup="listbox"
@@ -65,11 +41,11 @@ export default function SimpleListMenu() {
           aria-label="when device is locked"
           aria-expanded={open ? 'true' : undefined}
           onClick={handleClickListItem}
-        >
+          >
           <ListItemText
             primary="Choose your Data"
             secondary={dataOptions[selectedIndex]}
-          />
+            />
         </ListItemButton>
       </List>
       
@@ -82,12 +58,12 @@ export default function SimpleListMenu() {
           'aria-labelledby': 'lock-button',
           role: 'listbox',
         }}
-      >
+        >
         {dataOptions.map((option, index) => (
           <MenuItem
-            key={option}
-            selected={index === selectedIndex}
-            onClick={(event) => handleMenuItemClick(event, index)}
+          key={option}
+          selected={index === selectedIndex}
+          onClick={(event) => handleMenuItemClick(event, index)}
           >
             {option}
           </MenuItem>
--- src/sections/upload/view/tradeRepublic-view.jsx
+++ src/sections/upload/view/tradeRepublic-view.jsx
@@ -10,11 +10,11 @@ import TableContainer from '@mui/material/TableContainer';
 import TablePagination from '@mui/material/TablePagination';
 
 // import { tradeRepublic } from 'src/_mock/tradeRepublic';
-
 import Scrollbar from 'src/components/scrollbar';
+
 import DataInput from '../dataInput';
 import SimpleListMenu from '../dataInputTest';
-import UploadFile from '../uploadButton';
+// import UploadFile from '../uploadButton';
 
 import TableNoData from '../table-no-data';
 import TableEmptyRows from '../table-empty-rows';
@@ -92,35 +92,14 @@ export default function TradeRepublicPage() {
     data.flatMap(x => Object.keys(x)).filter((value, index, array) => array.indexOf(value) === index)
 
 
-  // const dataPage = 'http://localhost:8080/ids';
-  // const dataPage = array von lokal host8080 listOfIDs
-  const [allDataPages, setAllDataPages] = useState([]);
-
-  // const dataPage = 'finance.yahoo.com.stock.factory';  // 'boerse.de.stock.factory'; // 'onvista.de.stock.factory'; 
   // seite von der die daten gerade kommen
-  const [dataPage, setDataPage] = useState('finance.yahoo.com.stock.factory');
-
-  useEffect(() => {
-    // url die gelesen wird
-    fetch('http://127.0.0.1:8080/ids')
-      // ist ne json datei
-      .then((response) => response.json())
-      // nimm die daten die(momentan noch) im data.config.baseUrls array stehen
-      .then((data) => {
-        console.log(data);
-        setAllDataPages(data);
-      })
-      // exeption
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }, []);
-
+  // const [dataPage, setDataPage] = useState('finance.yahoo.com.stock.factory'); 
   // zieht die aktuellen daten aus dem netz von den unterschiedlichen seiten
   const [newData, setNewData] = useState([]);
-
+  
   useEffect(() => {
     // url die gelesen wird
+    // conact with data from dataInputTest
     fetch('http://127.0.0.1:8080/config?factoryId='.concat('boerse.de.stock.factory'))
       // ist ne json datei
       .then((response) => response.json())
@@ -135,8 +114,9 @@ export default function TradeRepublicPage() {
       });
   }, []);
 
-  // welche spalten werden angezeigt( reihenfolge, anzahl,..)
-  // const showCols = ["id", "path", "pattern", "url", "name", "type"];
+
+  // var die die aus der drop down tabelle die ausgewählte seite wieergeben soll
+  // const choosenWebsite =
 
   // aus den json daten alle erwähnten columns
   const showCols = allCols(newData)
@@ -147,13 +127,32 @@ export default function TradeRepublicPage() {
   // const headlineCols = ColumnsOfRows(newData)
 
   const dataFiltered = applyFilter({
-    //  inputData: tradeRepublic,
-    inputData: newData,
+    inputData: selectedIndex,
     comparator: getComparator(order, orderBy),
     filterName,
   });
 
   const notFound = !dataFiltered.length && !!filterName;
+
+  const [dataOptions, setAllDataPages] = useState([]);
+
+    useEffect(() => {
+      // url die gelesen wird
+      fetch('http://localhost:8080/factoryIds')
+        // ist ne json datei
+        .then((response) => response.json())
+        // nimm die daten die(momentan noch) im data.config.baseUrls array stehen
+        .then((data) => {
+          console.log(data);
+          setAllDataPages(data);
+        })
+        // exeption
+        .catch((err) => {
+          console.log(err.message);
+        });
+    }, []);
+
+    const [selectedIndex, setSelectedIndex] = useState();
 
   return (
     <Container>
@@ -169,7 +168,11 @@ export default function TradeRepublicPage() {
         />
 
         <DataInput />
-        <SimpleListMenu/>
+        <SimpleListMenu 
+        dataOptions = {dataOptions}
+        selectedIndex = {selectedIndex}
+        setSelectedIndex = {setSelectedIndex}
+        />
 
         <Scrollbar>
           <TableContainer sx={{ overflow: 'unset' }}>
