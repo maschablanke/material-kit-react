d7406940611f40e9c3c96f1b76c812e97bd46ecb
Mascha Blanke <maschablanke@gmx.de>
2024-04-24 17:42:04 +0200

    verallgemeinern

--- src/sections/oma/view/oma-view.jsx
+++ src/sections/oma/view/oma-view.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Unstable_Grid2';
@@ -5,9 +6,18 @@ import Typography from '@mui/material/Typography';
 
 import AppWebsiteVisits from '../app-website-visits';
 import AppWidgetSummary from '../app-widget-summary';
-// ----------------------------------------------------------------------
+import AppCurrentVisits from '../app-current-visits';
 
+// ----------------------------------------------------------------------
 export default function AppView() {
+
+  
+  const [currentTotal, setCurrentTotal] = useState(12000);
+  const [currentInvest, setCurrentInvest] = useState(10000);
+  const [currentInterest, setCurrentInterest] = useState(2000);
+
+
+
   return (
     <Container maxWidth="xl">
       <Typography variant="h4" sx={{ mb: 5 }}>
@@ -18,7 +28,7 @@ export default function AppView() {
         <Grid xs={12} sm={6} md={3}>
           <AppWidgetSummary
             title="Total"
-            total={714000}
+            total={currentTotal}
             color="success"
             icon={<img alt="icon" src="/assets/icons/glass/ic_glass_bag.png" />}
           />
@@ -27,7 +37,7 @@ export default function AppView() {
         <Grid xs={12} sm={6} md={3}>
           <AppWidgetSummary
             title="Investiert"
-            total={1352831}
+            total={currentInvest}
             color="info"
             icon={<img alt="icon" src="/assets/icons/glass/ic_glass_users.png" />}
           />
@@ -35,8 +45,8 @@ export default function AppView() {
 
         <Grid xs={12} sm={6} md={3}>
           <AppWidgetSummary
-            title="4%"
-            total={1723315}
+            title="Festgeld"
+            total={currentInterest}
             color="warning"
             icon={<img alt="icon" src="/assets/icons/glass/ic_glass_buy.png" />}
           />
@@ -44,7 +54,7 @@ export default function AppView() {
 
         <Grid xs={12} md={6} lg={8}>
           <AppWebsiteVisits
-            title="Website Visits"
+            title="Übersicht"
             subheader="(+43%) than last year"
             chart={{
               labels: [
@@ -62,23 +72,35 @@ export default function AppView() {
               ],
               series: [
                 {
-                  name: 'Team A',
+                  name: 'Gesamt',
                   type: 'column',
                   fill: 'solid',
                   data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
                 },
                 {
-                  name: 'Team B',
+                  name: 'Investiert',
                   type: 'area',
                   fill: 'gradient',
                   data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
                 },
                 {
-                  name: 'Team C',
+                  name: 'Festgeld',
                   type: 'line',
                   fill: 'solid',
                   data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
                 },
+              ],
+            }}
+          />
+        </Grid>
+
+        <Grid xs={12} md={6} lg={4}>
+          <AppCurrentVisits
+            title="Übersicht"
+            chart={{
+              series: [
+                { label: 'Invest', value: currentInvest },
+                { label: 'Festgeld', value: currentInterest },
               ],
             }}
           />
